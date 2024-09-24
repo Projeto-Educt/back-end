0bce1c638713d0d193aa4d2628ef8145b0a5e258
staviasz <erickesc21@hotmail.com>
2024-09-24 19:13:17 -0300

    chore: tests setup

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            decorators: true,
+          },
+          transform: {
+            decoratorMetadata: true,
+          },
+        },
+      },
+    ],
+  },
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+  verbose: true,
+  passWithNoTests: true,
+  noStackTrace: true,
+  testSequencer: './test-sequencer.js',
+};
